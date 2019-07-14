178ae612978248160e4340375b64517b2b4c1f94
Alex Lu <alexlu.manhasset@gmail.com>
2019-07-14 11:42:47 -0400

    project1 everything working

--- project1/application.py
+++ project1/application.py
@@ -1,4 +1,5 @@
 import os
+import csv
 from flask import Flask, request, render_template, redirect, url_for, session
 from flask_session import Session
 from sqlalchemy import create_engine
@@ -20,10 +21,23 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-
+def select(target, table, input, info):
+    return db.execute("SELECT {} FROM {} WHERE {} = '{}';"
+    .format(target, table, input, info)).fetchone()
 
 @app.route("/")
 def index():
+    #read csv file
+    f = open("books.csv")
+    reader = csv.reader(f)
+    number_books = db.execute("SELECT * FROM books;").fetchall()
+    if number_books == []:
+        for isbn, title, author, year in reader:
+            db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+            {"isbn": isbn, "title": title, "author": author, "year": year})
+        db.commit()
+
+    session["user_id"] = None
     # if "message" in session:
     #     message = session["message"]
     # else:
@@ -38,18 +52,39 @@ def index():
 def login():
     username = request.form.get("username")
     password = request.form.get("password")
-    result = db.execute("SELECT * FROM login WHERE username = username AND password = password").fetchall()
-    if not result:
+    result = db.execute("SELECT * FROM login WHERE username = '{username}' AND password = '{password}' "
+    .format(username = username, password = password)).fetchall()
+    if len(result) == 0:
         message = "Username and/or Password is incorrect!"
         # session["message"] = message
         # return redirect(url_for("index"))
         return redirect(url_for("index", message = message))
     else:
+        row = select("id", "login", "username", username)
+        session["user_id"] = row[0]
         return redirect(url_for("success"))
 
-@app.route("/success")
+@app.route("/dashboard", methods=['GET', 'POST'])
 def success():
-    return render_template("success.html")
+    #get username
+    row = select("username", "login", "id", session["user_id"])
+    username = row[0]
+
+    methods = request.form.get('search_methods')
+    search = request.form.get('search')
+
+
+    if methods in ("author", "title"):
+        methods = "UPPER({})".format(methods)
+        search = search.upper()
+
+    books = []
+    if methods and search:
+        db_str = "SELECT * FROM books WHERE {methods} LIKE '%{search}%';".format(
+            methods = methods, search = search)
+        print(db_str)
+        books = db.execute(db_str).fetchall()
+    return render_template("success.html", username = username, books = books)
 
 @app.route("/register")
 def register():
@@ -64,11 +99,19 @@ def insert():
     new_password = request.form.get("password")
     confirm_password = request.form.get("confirm_password")
 
-    if new_password == confirm_password:
-        db.execute("INSERT INTO login (username, password) VALUES ('{username}', '{password}'); "
-        .format(username=new_username, password=new_password))
-        db.commit()
+    taken_usernames = db.execute("SELECT username FROM login").fetchall()
+    for x in taken_usernames:
+        if new_username == x[0]:
+            message = "Username already taken"
+            return redirect(url_for("register", message = message))
 
+    if new_password == confirm_password:
+        db.execute("INSERT INTO login(username, password) VALUES('{username}', '{password}')"
+        .format(username = new_username, password = new_password))
+        db.commit()
+        row = select("id", "login", "username", new_username)
+        session["user_id"] = row[0]
         return redirect(url_for("success"))
+
     message = "Passwords do not match."
     return redirect(url_for("register", message = message))
